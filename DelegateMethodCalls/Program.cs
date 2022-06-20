72b0f40252289145efae659627de792373297408
BelowSeaLevel <107874678+BelowSeaLevel@users.noreply.github.com>
2022-06-20 20:12:53 +0200

    Added Project files and folders
    Added Project files and folders

---
+++ DelegateMethodCalls/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateMethodCalls
+{
+    class Program
+    {
+
+        delegate void UseDelegateToCallMethods(string a);
+        delegate void LetsTryToCalculate(int a, int b);
+        delegate void LetsTryStringAndInt(string a, int b);
+
+        static void Main()
+        {
+            UseDelegateToCallMethods callMethods;
+
+            callMethods = SayHello;
+            callMethods("Hello");
+            callMethods = SayGoodBye;
+            callMethods("Goodbye");
+
+            LetsTryToCalculate callCalculate;
+            callCalculate = AddTwo;
+            callCalculate += Multiply;
+            callCalculate(5, 8);
+
+            LetsTryStringAndInt stringAndInt;
+            stringAndInt = StringAndInt;
+            stringAndInt("Hey", 88);
+
+
+            Console.ReadKey();
+
+        }
+
+
+        static void SayHello(string a)
+        {
+            Console.WriteLine(a);
+        }
+
+
+        static void SayGoodBye(string a)
+        {
+            Console.WriteLine("Goodbye");
+        }
+
+        static void AddTwo(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        static void Multiply(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+
+        static void StringAndInt(string a, int b)
+        {
+            Console.WriteLine($"{a} = a string, and {b} = a int");
+        }
+    }
+}
